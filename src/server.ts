b8aa30f94833a747d7a600ba1229c8dd35a7c056
levi8102 <iamgourabdg2002@gmail.com>
2026-08-14 10:53:15 +0530

    feat: add database connection and auth setup

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import { PORT } from "./configs/server.config.js";
 import { logger } from "./configs/logger.config.js";
 import { connectToDatabase } from "./configs/db.config.js";
 
+// to check if commit works or not 
+
 async function startServer() {
     await connectToDatabase();
     app.listen(PORT, () => {
