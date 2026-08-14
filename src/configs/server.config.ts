b57b13cacc33a900e66ac4f78e593c83dc995113
shreeya-07 <shreeyachattopadhyay@gmail.com>
2026-08-14 20:24:30 +0530

    Implement secure password hashing for user signup

--- src/configs/server.config.ts
+++ src/configs/server.config.ts
@@ -7,3 +7,7 @@ export const DB_HOST = process.env.DB_HOST || 'localhost';
 export const DB_USER = process.env.DB_USER || 'root';
 export const DB_NAME = process.env.DB_NAME || 'task_system';
 export const DB_PASSWORD = process.env.DB_PASSWORD || '1748arijiT#'; 
+
+
+export const BCRYPT_SALT =
+    Number(process.env.BCRYPT_SALT) || 10;
