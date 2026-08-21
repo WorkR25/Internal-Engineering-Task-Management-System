a6a334eb5e543ba2c5a5f2986a26edea0ebf3e16
levi8102 <iamgourabdg2002@gmail.com>
2026-08-21 18:56:18 +0530

    api 1 and 2 of performance implemented with dummy data

--- src/routes/v1/performance.route.ts
+++ src/routes/v1/performance.route.ts
@@ -1,10 +1,28 @@
 import { Router } from "express";
-
 import { PerformanceController } from "../../controllers/performance.controller.js";
 import { PerformanceService } from "../../services/performance.service.js";
+import { authenticateUser } from "../../middlewares/authentication.middleware.js";
+import { authorizeUser } from "../../middlewares/authorization.middleware.js";
+import { RoleName } from "../../types/role.type.js";
 
-const performanceController = new PerformanceService();
+const performanceController = new PerformanceController(new PerformanceService());
 
 export const performanceRouter = Router();
 
 // implement all the routes below here
+performanceRouter.get(
+    "/me",
+    authenticateUser,
+    authorizeUser(RoleName.DEVELOPER),
+    performanceController.getCurrentUserPerformanceHandler.bind( performanceController)
+);
+
+
+performanceRouter.get(
+    "/me/tasks",
+    authenticateUser,
+    authorizeUser(RoleName.DEVELOPER),
+    performanceController
+        .getCurrentUserTaskLevelPerformanceHandler
+        .bind(performanceController)
+);
